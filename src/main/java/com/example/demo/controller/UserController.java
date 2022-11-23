2391f7572a6de83185491b08cc189711006eb911
최성우/SRE팀 <seongwoo.choi@musinsa.com>
2022-11-23 21:56:39 +0900

    first commit

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    // 회원가입
+    @GetMapping("/")
+    public String join() {
+        return "hello";
+    }
+
+}
